86df31a5f829bdfaefe97b5f288ce02753228837
saria3321 <153485612+saria3321@users.noreply.github.com>
2024-03-27 13:47:59 +0500

    Add files via upload

---
+++ FobBus-User-Interface/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\Dev_tools\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Laptop House\\Desktop\\Open Source Project\\Practice_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\Dev_tools\\flutter"
+  "PROJECT_DIR=C:\\Users\\Laptop House\\Desktop\\Open Source Project\\Practice_app"
+  "FLUTTER_ROOT=D:\\Dev_tools\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Laptop House\\Desktop\\Open Source Project\\Practice_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Laptop House\\Desktop\\Open Source Project\\Practice_app"
+  "FLUTTER_TARGET=C:\\Users\\Laptop House\\Desktop\\Open Source Project\\Practice_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8zZjNlNTYwMjM2NTM5YjdlMjcwMmY1YWM3OTBiMmE0NjkxYjMyZDQ5Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Laptop House\\Desktop\\Open Source Project\\Practice_app\\.dart_tool\\package_config.json"
+)
